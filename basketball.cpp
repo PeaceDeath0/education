6a6f6dc11fc74476b7ebe8640b0b3f117995df63
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-01-25 11:53:43 +0700

    Create basketball.cpp

---
+++ basketball.cpp
@@ -0,0 +1,34 @@
+//НЕ ЗАКОНЧИЛ
+#include <iostream>
+
+void victories() {
+    int score1, score2;
+    
+    if (*action1==A & *zone==3)
+    score1+3
+    printf(score1"-"score2)
+    else (*zone==2)
+    score1+2
+    print(score1"-"score2)
+    
+    if (*action2==A & *zone==3)
+    score2+3
+    printf(score1"-"score2)
+    else (*zone==2)
+    score2+2
+    
+    if (score1==25 and score1-score2=>2)
+    printf("Team A victory! Score:\n"score1""score2)
+    set++
+    elif if(score2=23 and score2-score1=>2)
+    printf("Team B victory! Score:\n"score1""score2)
+    set++
+    if (set==3)
+    printf("Game over")
+}
+void teams(){
+    int team1, team2
+    int team1[8]=[2,5,6,7,19,10,5,9,3,14]
+    int team2[8]=[1,4,12,15,16,17,18,8,11]
+    
+}
