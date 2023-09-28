db7c863ae6f09627a86f2babbdd8ca324ebbe638
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-09-28 13:32:06 +0700

    Update bankCredit.cpp

--- bankCredit.cpp
+++ bankCredit.cpp
@@ -1,63 +1,71 @@
 #include <iostream>
 #include <cmath>
 
-    double calculateAnnuityPayment(double loanAmount, int term, double interestRate) {
-        double monthlyInterestRate = interestRate / 12 / 100; // Процентная ставка в месяц
-        double coefficient = monthlyInterestRate * pow(1 + monthlyInterestRate, term) / (pow(1 + monthlyInterestRate, term) - 1);
-        return loanAmount * coefficient;
+using namespace std;
+
+double calculateAnnuityPayment(double loanAmount, int term, double interestRate) {
+    double monthlyInterestRate = interestRate / 12 / 100; // Процентная ставка в месяц
+    cout << "monthlyInterestRate " << monthlyInterestRate;
+    double numerator = loanAmount * monthlyInterestRate * pow(1 + monthlyInterestRate, term);
+    double coefficient = pow(1 + monthlyInterestRate, term) - 1;
+    return numerator / coefficient;
+}
+
+void differentialPayment(double loanAmount, double interestRate, int term) {
+    double monthlyInterestRate = interestRate / 12 / 100;
+    double monthlyLA = loanAmount / term;
+
+    for (int i = 1; i <= term; i++) {
+        double monthlyPayment = monthlyLA + loanAmount * monthlyInterestRate;
+        cout << "Месяц " << i << ":" << monthlyPayment << " рублей" << endl;
+        loanAmount -= monthlyLA;
+    }
+}
+
+double calculateOverpayment(double monthlyPayment, int term, double loanAmount) {
+    return monthlyPayment * term - loanAmount;
+}
+
+double calculateTotalPayment(double monthlyPayment, int term) {
+    return monthlyPayment * term;
+}
+
+int main() {
+    double loanAmount;
+    int term;
+    double interestRate;
+    std::string type;
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите общую сумму кредита: ";
+    cin >> loanAmount;
+    cout << "Введите срок кредита (в месяцах): ";
+    cin >> term;
+    cout << "Введите процентную ставку: ";
+    cin >> interestRate;
+    cout << "Введите тип кредита (аннуитетный или дифференцированный): ";
+    cin >> type;
+
+    double monthlyPayment;
+    double overpayment;
+    double totalPayment;
+
+    if (type == "a") {
+        monthlyPayment = calculateAnnuityPayment(loanAmount, term, interestRate);
+        overpayment = calculateOverpayment(monthlyPayment, term, loanAmount);
+        totalPayment = calculateTotalPayment(monthlyPayment, term);
+    }
+    else if (type == "b") {
+        differentialPayment(loanAmount, interestRate, term);
+        return 1;
+    }
+    else {
+        cout << "Неверно указан тип кредита." << endl;
+        return 1;
     }
 
-    double calculateDifferentiatedPayment(double loanAmount, int term, double interestRate, int month) {
-        double monthlyInterestRate = interestRate / 12 / 100; // Процентная ставка в месяц
-        double bodyPayment = loanAmount / term; // Сумма выплаты по основному долгу в месяц
-        double interestPayment = (loanAmount - (month - 1) * bodyPayment) * monthlyInterestRate; // Сумма выплаты по процентам в месяц
-        return bodyPayment + interestPayment;
-    }
+    cout << "Ежемесячный платеж: " << monthlyPayment << endl;
+    cout << "Переплата по кредиту: " << overpayment << endl;
+    cout << "Общая выплата: " << totalPayment << endl;
 
-    double calculateOverpayment(double monthlyPayment, int term, double loanAmount) {
-        return monthlyPayment * term - loanAmount;
-    }   
-
-    double calculateTotalPayment(double monthlyPayment, int term) {
-        return monthlyPayment * term;
-    }
-
-    int main() {
-        double loanAmount;
-        int term;
-        double interestRate;
-        std::string type;
-
-        std::cout << "Введите общую сумму кредита: ";
-        std::cin >> loanAmount;
-        std::cout << "Введите срок кредита (в месяцах): ";
-        std::cin >> term;
-        std::cout << "Введите процентную ставку: ";
-        std::cin >> interestRate;
-        std::cout << "Введите тип кредита (аннуитетный или дифференцированный): ";
-        std::cin >> type;
-
-        double monthlyPayment;
-        double overpayment;
-        double totalPayment;
-
-        if (type == "аннуитетный") {
-            monthlyPayment = calculateAnnuityPayment(loanAmount, term, interestRate);
-            overpayment = calculateOverpayment(monthlyPayment, term, loanAmount);
-            totalPayment = calculateTotalPayment(monthlyPayment, term);
-        } else if (type == "дифференцированный") {
-            monthlyPayment = calculateDifferentiatedPayment(loanAmount, term, interestRate, 1);
-            overpayment = calculateOverpayment(monthlyPayment, term, loanAmount);
-            totalPayment = calculateTotalPayment(monthlyPayment, term);
-        } else {
-            std::cout << "Неверно указан тип кредита." << std::endl;
-            return 0;
-        }
-                
-        std::cout << "Ежемесячный платеж: " << monthlyPayment << std::endl;
-        std::cout << "Переплата по кредиту: " << overpayment << std::endl;
-        std::cout << "Общая выплата: " << totalPayment << std::endl;
-
-        return 0;
-    }
-
+    return 0;
