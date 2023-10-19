c1a1c903ccb2fb0ac189f298ce1b2388227d26cf
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-10-19 11:45:36 +0700

    Update Stolbik.cpp

--- Stolbik.cpp
+++ Stolbik.cpp
@@ -1,48 +1,64 @@
 #include <iostream>
-using namespace std;
+#include <sstream>
 
-int main()
-{
-    // Создаем массив для хранения чисел
-    int numbers[3];
+// Функция для ввода 100 значных чисел через буфер
+void inputNumbers(int buffer[100]) {
+    std::string input;
 
-    // Вводим значения для массива
-    cout << "Введите три 3-х значных числа: ";
-    for (int i = 0; i < 3; i++)
-    {
-        cin >> numbers[i];
+    // Запрашиваем у пользователя ввод чисел
+    std::cout << "Введите 100 значных чисел через пробел: ";
+    std::getline(std::cin, input);
+
+    // Используем строковый поток для разделения чисел
+    std::istringstream iss(input);
+
+    // Заполняем буфер
+    for (int i = 0; i < 100; i++) {
+        iss >> buffer[i];
     }
+}
+
+// Функция для вывода массива типа int
+void printArray(int array[100]) {
+    for (int i = 0; i < 100; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Создаем буфер и массив для вывода
+    int buffer[100];
+    int array[100];
+
+    // Вызываем функцию ввода чисел
+    inputNumbers(buffer);
 
     // Выводим массив типа int
-    cout << "Массив: ";
-    for (int i = 0; i < 3; i++)
-    {
-        cout << numbers[i] << " ";
-    }
-    cout << endl;
+    std::cout << "Массив типа int: ";
+    printArray(buffer);
 
-    // Выполняем сложение и вычитание чисел
+    // Выполняем сложение и вычитание
     int sum = 0;
-    int difference = numbers[0];
-    for (int i = 1; i < 3; i++)
-    {
-        sum += numbers[i];
-        difference -= numbers[i];
+    int difference = 0;
+
+    for (int i = 0; i < 100; i++) {
+        sum += buffer[i];
+        difference -= buffer[i];
     }
 
-    // Выводим результаты
-    cout << "Сумма чисел: " << sum << endl;
-    cout << "Разность чисел: " << difference << endl;
+    std::cout << "Сумма чисел: " << sum << std::endl;
+    std::cout << "Разность чисел: " << difference << std::endl;
 
-    // Выполняем умножение чисел
-    int multiplication = 1;
-    for (int i = 0; i < 3; i++)
-    {
-        multiplication *= numbers[i];
+    // Выполняем умножение
+    int product = 1;
+
+    for (int i = 0; i < 100; i++) {
+        product *= buffer[i];
     }
 
-    // Выводим результат умножения
-    cout << "Умножение чисел: " << multiplication << endl;
+    std::cout << "Произведение чисел: " << product << std::endl;
 
     return 0;
 }
+
